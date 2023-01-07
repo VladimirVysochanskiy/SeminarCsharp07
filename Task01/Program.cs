b292e1882c28cddf95a9a28252829760da9707f8
VladimirVysochanskiy <shunkar78@mail.ru>
2023-01-07 14:18:44 +0300

    Add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве 
+находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5 */
+// --------------------------------------------------------------------
+
+int[,] myArray =new int[5,5];
+FillArray2DIndexSum(myArray);
+PrintArray2D(myArray);
+System.Console.WriteLine();
+
+//Метод Печать 2D массива равнение право.
+PrintArray2D(CreateFill2DArrayRandom(10, 10, -99, 99));
+
+
+
+void FillArray2DIndexSum(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = i + j;
+        }
+    }
+}
+
+void PrintArray2D(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            //System.Console.Write($" {matr[i, j]} ");
+            System.Console.Write(String.Format("{0,4}", matr[i, j]));
+        }
+        System.Console.WriteLine();
+    }
+}
+
+//Метод Создание и рандомное наполнение 2D int массива
+int[,] CreateFill2DArrayRandom(int strings, int columns, int fromNum, int toNum)
+{
+    int[,] array = new int[strings, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(fromNum, toNum);
+        }
+    }
+    return array;
+}
+
+
+/*
+dynamic result(int x)
+{
+    if (x == 1) return 58;
+    else if (x == 2) return "String";
+    else return true;
+}
+*/
